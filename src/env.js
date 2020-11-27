9b1f5f4254ad870232de707671766925bf424651
Suyash Lawand <suyashlawand@pop-os.localdomain>
2020-11-27 22:01:08 +0530

    domain update reconfigure

--- src/env.js
+++ src/env.js
@@ -1,2 +1,2 @@
 export const baseURL = "https://safe-crag-22767.herokuapp.com/"
-export const frontURL = "https://happy-wing-2c01bf.netlify.app/"
+export const frontURL = "https://answermyquestion.tk/"
